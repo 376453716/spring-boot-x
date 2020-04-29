4fa68119bbe569d503541cbb9e39587ddb16c4ca
xionghao <xionghao@xionghao-Mac.local>
2020-04-29 14:29:02 +0800

    migrate projec

---
+++ spring-boot-demo/src/main/java/com/xh/demo/controller/DemoController.java
@@ -0,0 +1,22 @@
+package com.xh.demo.controller;
+
+import com.xh.demo.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Xiong Hao
+ * @date 2020/4/6
+ */
+@RestController
+public class DemoController {
+
+	@Autowired
+	private DemoService demoService;
+
+	@GetMapping("/")
+	public String get() {
+		return demoService.getServiceName();
+	}
+}
